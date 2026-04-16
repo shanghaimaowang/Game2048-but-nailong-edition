8e8e71c0fbb14df5b9d91265c33832bad570fa46
CatKing of SH <3240718281@qq.com>
2026-04-16 21:52:29 +0800

    day3

--- game2048.py
+++ game2048.py
@@ -1,9 +1,10 @@
 import random
 import pygame
-import get_number_background
+from get_number_background import get_number_background
 
 
-class ezfe():
+
+class ezfe:
     #初始化棋盘
     def __init__(self,matrix_size=(4,4),max_score_filepath : int=None,**kwargs):
         self.matrix_size=matrix_size
@@ -15,7 +16,7 @@ class ezfe():
 
     def randomgenerateNumber(self):
         empty_pos=[]
-        for i in range(self.matrix.size[0]):
+        for i in range(self.matrix_size[0]):
             for j in range(self.matrix_size[1]):
                 if self.game_matrix[i][j]=="null":
                     empty_pos.append([i,j])
@@ -32,20 +33,39 @@ class ezfe():
             self.randomgenerateNumber()
 
     def draw_rect(self,screen):
-        cell_size=100
-        gap=10
+        cell_size=104
+        gap=2
         start_x=0
         start_y=0
+        #测试格子打印效果
+        self.game_matrix[0][0]=128
         for i in range(self.matrix_size[0]):
             for j in range(self.matrix_size[1]):
                 x=start_x+i*(cell_size+gap)
                 y=start_y+j*(cell_size+gap)
                 val=self.game_matrix[i][j]
                 if val=="null":
-                    bg_color=(200, 190, 180)
+                    bg_color=(200, 190, 180,125)
                     pygame.draw.rect(screen, bg_color, (x, y, cell_size, cell_size), border_radius=8)
                 else:
-                    bg_color=(255, 255, 255, 255)
+                    num_background=get_number_background().get_number_background(val)
+                    mask=pygame.Surface((104,104),pygame.SRCALPHA)
+                    pygame.draw.rect(mask,(255,255,255),(0,0,104,104),border_radius=8)
+                    if val<100:
+                        sizeo=70
+                    elif val>=100 and val<1000:
+                        sizeo=60
+                    else:
+                        sizeo=55
+                    num=pygame.font.SysFont("kaiti", size=int(sizeo))
+                    num=num.render(str(val),True,(50,50,50,150))
+                    temp=num.get_rect(center=mask.get_rect().center)
+
+                    screen.blit(num_background,(x,y))
+                    screen.blit(mask,(x,y),special_flags=pygame.BLEND_RGBA_MIN)
+                    screen.blit(num,temp)
+
+
 
 
 
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from game2048 import ezfe
 #初始化游戏
 pygame.init()
 
-game_2048=ezfe()
 #创建窗口（画布）、时钟
 screen=pygame.display.set_mode(size=(600,430))
 pygame.display.set_caption("2048 for the best")
@@ -13,30 +12,36 @@ clock=pygame.time.Clock()
 
 #绘制背景
 background=pygame.image.load("background.png")
-background=pygame.transform.scale(background,(600,430))
+background=pygame.transform.scale(background,(430,430))
 overlay=pygame.Surface((600,430),pygame.SRCALPHA)
 overlay.fill((255,255,255, 128))
+pygame.draw.rect(overlay,(150,150,150),(430,0,170,430))
 
-#将格子绘制在画布上
-
+#游戏区域与控制区域分割线
+pygame.draw.line(overlay,(0,0,0),(430,0),(430,430),2)
 
 #开始按钮
-start_rect = pygame.Rect(440, 50, 90, 50)
+start_rect = pygame.Rect(470, 50, 90, 50)
 start_background=pygame.Surface(start_rect.size,pygame.SRCALPHA)
-
+pygame.draw.rect(start_background,(0,0,0,225),start_background.get_rect(),border_radius=24)
 start_font=pygame.font.SysFont("kaiti",30,True)
 start_font=start_font.render("开始",True,"black")
-
-
 temp_rect=start_font.get_rect(center=start_background.get_rect().center)
-pygame.draw.rect(start_background,(255, 255, 0),start_background.get_rect(),border_radius=8)
+pygame.draw.rect(start_background,(255, 255, 0),(2,2,86,46),border_radius=24)
 start_background.blit(start_font,temp_rect)
+if_get_start=False
 
-ifgetstart=False
+#开始按钮测试
 overlay_surface = pygame.Surface((600, 430), pygame.SRCALPHA)
 overlay_surface.fill((0, 0, 0, 128))
+
+
+
+
+
 #游戏进程主循环
 is_running=True
+game_2048 = ezfe()
 while is_running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -44,13 +49,16 @@ while is_running:
             sys.exit()
         if event.type==pygame.MOUSEBUTTONDOWN:
             if start_rect.collidepoint(event.pos):
-                ifgetstart=True
+                if_get_start=True
 
 
     screen.blit(background, (0, 0))
     screen.blit(overlay,(0,0))
     screen.blit(start_background, start_rect)
-    if ifgetstart:
-        screen.blit(overlay_surface, (0, 0))
+    if if_get_start:
+        #游戏显示画布
+        game_main_surface=pygame.Surface((430,430),pygame.SRCALPHA)
+        game_2048.draw_rect(game_main_surface)
+        screen.blit(game_main_surface, (0, 0))
     pygame.display.flip()
     clock.tick(60)
